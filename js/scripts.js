0ddfa56a63b7ea44f05cd48189160da656cf9f31
F3NJ0 <58293913+F3NJ0@users.noreply.github.com>
2022-02-02 13:17:08 +0100

    Added function to add Items to list

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,27 @@
-let list = $('#list');
-let li = $('<li></li>');
+function newItem() {
 
-list.append(li);
+  let input = $('#input');
+  let list = $('#list');
+  let inputValue = input.val();
+
+  // add item to list when clicking on "Add" button
+  let li = $('<li>' + inputValue + '</li>');
+  list.append(li);
+
+  // Strike through items when user clicks on it
+  function crossOut() {
+    li.on("click", function() {
+      li.addClass("strike");
+    });
+  }
+
+  // Adding delete button to list items
+  let deleteButton = $('<button>X</button>');
+  deleteButton.addClass("crossOutButton");
+  li.append(deleteButton);
+
+  // Delete items from List
+  function deleteListItem() {
+
+  }
+}
